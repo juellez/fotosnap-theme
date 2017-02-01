f0af410fc007ed321da043b2554ce9a48ba3891c
jewel mlnarik <jewel@mlnarik.com>
2017-01-31 21:00:28 -0800

    update venue types

--- page-venues.php
+++ page-venues.php
@@ -3,27 +3,10 @@
  * Template Name: Location / Venue
  */
 get_header();
-
-$venue = false;
-$active = false;
-$zozi_id = 0;
-
-if( is_page() && $post->post_parent > 0 ) { 
-  // post has parents
-  $venue = true;
-  $zozi_id = get_post_meta( $post->ID, 'zozi_product_id', true );
-  $active = has_term( 'active', 'status', $post );
-}
 ?>
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main athena-page" role="main">
-
-        <div class="portfolionav clearfix">
-            <a href="/photographers/mike/" rel="prev" title="Mike"><i class="icon-arrow-left"></i></a>
-            <a href="http://fotosnap.co/photographers/"><i class="icon-grid"></i></a> 
-            <a href="http://fotosnap.co/portfolio/katelyn/" rel="next" title="Katelyn"><i class="icon-arrow-right"></i></a>
-        </div>
 
         <?php while (have_posts()) : the_post(); ?>
 
@@ -33,20 +16,14 @@ if( is_page() && $post->post_parent > 0 ) {
                     <div class="dark-overlay-text">
 
                         <header class="entry-header center">
-                            <small><a href="/venues/">Pop-Up Studio <?= $venue ? "Locations" : "" ?></a></small>
+                            <small><a href="/venues/">Pop-Up Photo Studio <?= $venue ? "Locations" : "" ?></a></small>
                             <h1 class="entry-title">
                                 <?php the_title() ?>
                             </h1>
-                        </header><!-- .entry-header -->
-
-                        <?php // see if there's a booking button to display
-                            if( $active && $zozi_id ): ?>
-                        <script src="https://a.zozi.com/assets/widgets/bookit.js"></script>
-                        <div class="center">
-                            <div id="zozi_advance_activity_<?=$zozi_id ?>" class="zozi-advance-button-container"><a class="ga-track athena-button primary large"
-                                href="https://a.zozi.com/#/express/fotosnapor/products/<?=$zozi_id ?>" target="_blank">Book It!</a></div>
-                        </div>
-                        <?php endif; ?>
+                            <?php if( $active && $zozi_id ): ?>
+                              <?php the_content(); ?>
+                            <?php endif; ?>
+                        </header>
 
                         <?php // see if there's a cta to throw into this header space
                             $cta_url = get_post_meta( $post->ID, 'cta_url', true );
@@ -67,24 +44,8 @@ if( is_page() && $post->post_parent > 0 ) {
 
 
             <div class="row">
-                
                 <div class="col-sm-3">
 
-                <?php if( $venue ):
-                    echo '<div class="row venue-gallery">';
-                    if( !$active ){
-                        echo '<br><br><small>Typically available weekends before 10am.</small>';
-                        echo '<a class="ga-track athena-button primary large"
-                                href="typeform" target="_blank">Request It!</a>';
-                        echo '<br><br><small>Typically available weekends before 10am.</small>';
-                    }
-                    $media = get_attached_media( 'image' );
-                    foreach($media as $img):
-                        $url = wp_get_attachment_image_src($img->ID,'thumbnail');
-                        echo '<div class="col-xs-6"><img src="'.$url[0].'" /></div>';
-                    endforeach;
-                    echo '</div>';
-                else: ?>
                     <small>Want to be the first to know when a new location is added?</small>
                     <br><br>
                     <a href="http://eepurl.com/bB6v6j"
@@ -99,58 +60,38 @@ if( is_page() && $post->post_parent > 0 ) {
                     <a href="host" class="athena-button default large">Learn More</a>
                     <br><br>
                     <br><br>
-                <?php endif; ?>
+
                 </div>
 
                 <div class="col-sm-8 col-sm-offset-1">
                     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                         <div class="entry-content">
-                            <?php if( $venue ): ?>
-                                <?php the_content(); ?>
 
-                                    <div class="callout">
-                                        <small>Whether you are online looking for a job or a date, a selfie is a bad way to make a good impression. Instead, book a FotoSnap mini photo session! We offer fun locations to fit your personality and convenient times to fit your schedule. Just $49 and a 15 minute session gets you a professional photo to use on all your online profiles.</small>
-                                    </div>
+                        <?php /* potential to pull straight from zozi 
+                            <iframe data-src="https://a.zozi.com/#/embed/fotosnapor/products-list?embedOptions=%7B%22merchantCode%22%3A%22fotosnapor%22%2C%22view%22%3A%22month%22%2C%22productIDs%22%3A%22%22%2C%22capacityDisplay%22%3A%22text%22%2C%22capacityText%22%3A%22BOOK%22%2C%22soldOutDisplay%22%3A%22text%22%2C%22soldOutText%22%3A%22Sold%20Out%22%2C%22includeName%22%3Anull%2C%22pastDatesBehavior%22%3A%22hide%22%2C%22allowAddonsPriceRollup%22%3A%22true%22%2C%22availabilityView%22%3A%22first%22%2C%22modalButtonText%22%3A%22Open%20Calendar%22%2C%22listButtonText%22%3A%22Browse%20%2B%20Book%22%2C%22buttonTextColor%22%3A%22%23FFFFFF%22%2C%22iconColor%22%3A%22%23000000%22%2C%22checkoutButtonColor%22%3A%22%23F0722C%22%2C%22accentColor%22%3A%22%23f37521%22%2C%22allowLocationHashControl%22%3A%22true%22%2C%22productContent%22%3A%22show%22%2C%22showProductsPage%22%3A%22hide%22%2C%22defaultOpenView%22%3A%22app.embed.merchant.calendarWidget%22%2C%22addonsPage%22%3A%22show%22%2C%22buttonBackground%22%3A%22%23FF8200%22%2C%22lowerMaxQuantitySelectableLimit%22%3A1000%2C%22upperMaxQuantitySelectableLimit%22%3A10000%7D" data-allow-hashchange="true" frameborder="0" id="zozi-embedded-checkout" scrolling="no" seamless="seamless" style="width: 100%; height: 500px; position: relative; top: -120px;"></iframe><script src="https://a.zozi.com/assets/widgets/embedded.origin.js"></script>
+                            */ ?>
 
-                                <?php
-                                wp_link_pages(array(
-                                    'before' => '<div class="page-links">' . esc_html__('Pages:', 'athena'),
-                                    'after' => '</div>',
-                                ));
-                                ?>
-                            <?php else: ?>
+                            <h2>The city is our photo studio.</h2>
+                            <p>We're excited to be popping up in beautiful cafes, stores and cityscapes around Portland! 
 
-                                <h2>Upcoming: Available for Booking</h2>
-                                <?php // the_content(); ?>
-                                <?php fs_show_children('active') ?>
+                            Find a location that suits your personality&mdash;and helps you stand out from the competition. It's a great way to explore and support awesome, local businesses, too. Looking good never felt so great.</p>
+
+                            <?php fs_show_venues('active') ?>
+
+                            <p>Prefer to <a href="/book">book your headshot appointment from a calendar</a>? You can do that, too!</p>
 
 
-                                <h2>Where We've Been ... and may return</h2>
-                                <?php // the_content(); ?>
-                                <?php fs_show_children('archived') ?>
+                            <h3>Where we've been ... and may return.</h3>
 
-                            <?php endif; ?>
-                        </div><!-- .entry-content -->
+                            <?php fs_show_venues('archived') ?>
 
-                        <footer class="entry-footer">
-                            <?php
-                            edit_post_link(
-                                    sprintf(
-                                            /* translators: %s: Name of current post */
-                                            esc_html__('Edit %s', 'athena'), the_title('<span class="screen-reader-text">"', '"</span>', false)
-                                    ), '<span class="edit-link">', '</span>'
-                            );
-                            ?>
-                        </footer><!-- .entry-footer -->
-
-                    </article><!-- #post-## -->
+                        </div>
+                    </article>
                 </div>
             </div>
-
         <?php endwhile; // End of the loop. ?>
 
-    </main><!-- #main -->
-    
+    </main><!-- #main -->    
 </div><!-- #primary -->
 
 <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us11.list-manage.com","uuid":"d40bd49ac434fceb17a599c6e","lid":"3da796abc2"}) })</script>
